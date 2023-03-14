aef5f5735cbc557b41c41134c64b18225bc1ee22
woodpie9 <79443040+woodpie9@users.noreply.github.com>
2023-03-14 19:22:15 +0900

    Save and commit

--- WoodnetBase/TCPSocket.h
+++ WoodnetBase/TCPSocket.h
@@ -1,7 +1,56 @@
+// ReSharper disable CppClangTidyClangDiagnosticInvalidUtf8
 #pragma once
 #include "WinSocket.h"
-class TCPSocket :
-    public WinSocket
+#include "StreamQueue.h"
+
+// 20230314 woodpie9 코드 생성 
+// WinSocket을 상속 받는 TCP 소켓을 묘사하는 클래스
+
+
+WOODNET_BEGIN
+
+class TCPSocket : public WinSocket
 {
+	struct TCPBuffer
+	{
+		
+	};
+
+public:
+	TCPSocket(const TCPSocket&) = delete;
+	TCPSocket& operator=(const TCPSocket&) = delete;
+
+	TCPSocket();
+	~TCPSocket();
+
+	bool	Connect(SOCKADDR_IN& remoteAddr);
+
+	bool	Listen();
+
+	int		Recv(char* pInBuf, int nLen, int& error, LPWSAOVERLAPPED pOv = NULL, LPWSAOVERLAPPED_COMPLETION_ROUTINE lpCompletionRoutine = NULL);
+	int		Send(char* pOutBuf, int nLen, int& error, LPWSAOVERLAPPED pOv = NULL, LPWSAOVERLAPPED_COMPLETION_ROUTINE lpCompletionRoutine = NULL);
+
+
+
+	bool SendUpdate();
+	bool RecvUpdate();
+
+	bool PostPacket(char* buffer, int nSize);
+	bool ReadPacket(char* buffer, int nSize);
+	bool PeekPacket(char* buffer, int nSize);
+
+	void Reset();
+
+protected:
+
+
+private:
+	TCPBuffer m_SendBuf;
+	TCPBuffer m_RecvBuf;
+
+	StreamQueue* m_pSendQ;
+	StreamQueue* m_pRecvQ;
 };
 
+
+WOODNET_END
--- WoodnetBase/UDPSocket.h
+++ WoodnetBase/UDPSocket.h
@@ -1,7 +1,32 @@
+// ReSharper disable CppClangTidyClangDiagnosticInvalidUtf8
 #pragma once
 #include "WinSocket.h"
-class UDPSocket :
-    public WinSocket
+
+// 20230314 woodpie9 코드 생성 
+// WinSocket을 상속 받는 UDP 소켓을 묘사하는 클래스
+
+WOODNET_BEGIN
+
+class UDPSocket : public WinSocket
 {
+	struct UDPBuffer
+	{
+
+	};
+
+public:
+	UDPSocket(const UDPSocket&) = delete;
+	UDPSocket& operator=(const UDPSocket&) = delete;
+
+	UDPSocket();
+	~UDPSocket();
+
+	int		RecvFrom(char* pInBuf, int nLen, int& error, SOCKADDR_IN& remoteAddr, LPWSAOVERLAPPED pOv = NULL);
+	int		SendTo(char* pOutBuf, int nLen, int& error, SOCKADDR_IN& remoteAddr, LPWSAOVERLAPPED pOv = NULL);
+
+
 };
 
+
+
+WOODNET_END
