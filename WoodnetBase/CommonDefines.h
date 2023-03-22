2be3fead5afc05b381f519f56d211a42892f4f29
woodpie9 <79443040+woodpie9@users.noreply.github.com>
2023-03-22 15:55:23 +0900

    protofol 추가, defines 수정

--- WoodnetBase/CommonDefines.h
+++ WoodnetBase/CommonDefines.h
@@ -4,4 +4,10 @@
 #define WOODNET_BEGIN	namespace woodnet {
 #define WOODNET_END		}
 
+#define DELEY_SERVER_ms 50
+#define MAX_NICKNAME_LEN 10
+#define MAX_CHATTING_LEN 30
+#define GAME_SER_KEY_LEN 20
+#define PROTOCOL_DECLARE struct
+
 typedef __int32 NetworkObjectID;
